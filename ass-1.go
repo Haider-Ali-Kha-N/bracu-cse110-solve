f77b0115bf31e77d64cb1e7cc71d0a0cf8d1a0e5
Haider-Ali-Kha-N <haiderk3102002@gmail.com>
2023-09-07 20:44:41 -0700

    added all the cse110 assignment

--- ass-1.go
+++ ass-1.go
@@ -1,7 +1,9 @@
 package main
+
 import "fmt"
+
 func main() {
-    fmt.Println("hello world")
-    fmt.Println("hello world. Allahu akbar.")
+	fmt.Println("hello world")
+	fmt.Println("hello world. Allahu akbar.")
 
 }
